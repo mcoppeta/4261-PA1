d15b2ea6ee93101d7baca1eb756df97f2a96a084
Michael Coppeta <mcoppeta@gmail.com>
2022-09-11 09:08:25 -0400

    Progress on network methods

--- server/app.js
+++ server/app.js
@@ -37,16 +37,17 @@ app.post('/user', urlencodedParser, async (req, res) => {
     try {
         const client = await pool.connect();
         const result = await client.query(`INSERT INTO users (username, password, notifications) VALUES (${req.body.username}, ${req.body.password}, ${req.body.notifications})`)
-        res.send(result)
+        res.json({'status': 'success'});
     } catch (err) {
-        console.error(err)
-        res.send("Error " + err)
+        console.error(err);
+        res.json("Invalid input - user may already exist");
     }
 });
 
 app.post('/user/login', urlencodedParser, async (req, res) => {
     try {
         const client = await pool.connect();
+        console.log(req.body)
         const result = await client.query(`SELECT password FROM users WHERE username=${req.body.username}`)
         const expected = result.rows[0]['password'] || null;
 
@@ -56,7 +57,8 @@ app.post('/user/login', urlencodedParser, async (req, res) => {
         }
 
         if (req.body.password == expected) {
-            let ret = {'status': 'Success'}
+            
+            let ret = {'status': 'success'}
             res.json(ret);
         } else {
             let ret = {'status': 'Incorrect username or password'};
