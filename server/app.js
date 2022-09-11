8cc29dcfb6ff1b11cd1960b8d9b5558f7a08c727
Michael Coppeta <mcoppeta@gmail.com>
2022-09-11 04:07:49 -0400

    Creates simple request to PostreSQL test_table and should return the rows

--- server/app.js
+++ server/app.js
@@ -1,12 +1,33 @@
-const express = require('express')
-const app = express()
-app.use(express.static('public'))
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+const { Pool } = require('pg')
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 app.get("/", function(req, res) {
     res.send("<h1>This is the Entry Point</h1>")
+});
+
+app.get("/db", async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM test_table');
+        const results = {'results': (result) ? result.rows : null};
+        res.send(results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 })
 
 app.listen(process.env.PORT || 3000, //necessary for deployment
     () => {
-        console.log("Started Server")
-    })
+        console.log("Started Server");
+    });
