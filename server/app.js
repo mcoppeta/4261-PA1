347a866feb0108e3c8d9db91b21e5557bf2ee822
Michael Coppeta <mcoppeta@gmail.com>
2022-09-11 20:31:48 -0400

    Swaps body to params for API route

--- server/app.js
+++ server/app.js
@@ -57,10 +57,10 @@ app.get("/teams/:team", async (req, res) => {
 
 //app.use(express.json());
 
-app.post('/teams/:team/sub/:username', urlencodedParser, async (req, res) => {
+app.post('/teams/:team/sub/:username', async (req, res) => {
     try {
         const client = await pool.connect();
-        const result = await client.query(`INSERT INTO subscriptions (username, team) VALUES ('${req.body.username}', '${req.body.team}')`)
+        const result = await client.query(`INSERT INTO subscriptions (username, team) VALUES ('${req.params.username}', '${req.params.team}')`)
         res.json({'status': 'success'});
     } catch (err) {
         console.error(err);
